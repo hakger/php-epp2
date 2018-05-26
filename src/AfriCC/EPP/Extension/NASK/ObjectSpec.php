aca2095d5404994378fc3869e4796db778cf25f8
Hubert Kowalski <h.kowalski@hakger.pl>
2018-05-26 17:30:25 +0200

    Add NASK ObjectSpec + Enhance late static binding for ObjectSpec

---
+++ src/AfriCC/EPP/Extension/NASK/ObjectSpec.php
@@ -0,0 +1,49 @@
+<?php
+namespace AfriCC\EPP\Extension\NASK;
+
+use AfriCC\EPP\ObjectSpec as MainObjectSpec;
+
+class ObjectSpec extends MainObjectSpec
+{
+
+    public static $spec = [
+        'epp' => [
+            'xmlns' => 'http://www.dns.pl/nask-epp-schema/epp-2.0',
+        ],
+        'domain' => [
+            'xmlns' => 'http://www.dns.pl/nask-epp-schema/domain-2.0',
+        ],
+        'host' => [
+            'xmlns' => 'http://www.dns.pl/nask-epp-schema/host-2.0'
+        ],
+        'contact' => [
+            'xmlns' => 'http://www.dns.pl/nask-epp-schema/contact-2.0'
+        ],
+        'future' => [
+            'xmlns' => 'http://www.dns.pl/nask-epp-schema/future-2.0'
+        ],
+    
+    ];
+    
+    public static $services = [
+        'http://www.dns.pl/nask-epp-schema/contact-2.0',
+        'http://www.dns.pl/nask-epp-schema/host-2.0',
+        'http://www.dns.pl/nask-epp-schema/domain-2.0',
+        'http://www.dns.pl/nask-epp-schema/future-2.0',
+    ];
+    
+    public static $serviceExtensions = [
+        'http://www.dns.pl/nask-epp-schema/extcon-2.0',
+        'http://www.dns.pl/nask-epp-schema/extdom-2.0',
+        'http://www.dns.pl/nask-epp-schema/extreport-2.0',
+        'http://www.dns.pl/nask-epp-schema/extdom-2.0',
+        'http://www.dns.pl/nask-epp-schema/secDNS-2.0',
+    ];
+    
+    public static function overwriteParent(){
+        MainObjectSpec::$specs = self::$spec;
+    }
+    
+    
+}
+
